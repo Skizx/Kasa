9658a2d140001c642a2e3a5bff5a178d1e1af133
Kevin <kevin.le.milanais@hotmail.fr>
2022-11-29 07:24:02 +0100

    Création Hostname&Hostpicture

--- src/components/FicheLogement.jsx
+++ src/components/FicheLogement.jsx
@@ -38,23 +38,28 @@ const FicheLogement = () => {
 
   return (
     logements && (
-        // Affichage Carrousel props
+      // Affichage Carrousel props
       <div key={params.id} className="fiche-container">
         <Carrousel slides={slidePics} />
         <div /* Affichage du titre ainsi que de la localisation */>
           <h1 className="fiche-container_title">{logements.title}</h1>
           <h2 className="fiche-container_location">{logements.location}</h2>
         </div>
-        <div className="taglist-container" /* Affichage de la liste de tags props */>
+        <div
+          className="taglist-container" /* Affichage de la liste de tags props */
+        >
           <ul className="taglist-container_position">{tags}</ul>
         </div>
-        <div>
-          <div>{logements.rating}</div>
-          <div className='user-container'>
-            <div className='user-container_name'>
-            {logements.host.name} 
-            </div>
-            <img src={logements.host.picture} alt="User profil" className='user-picture' />
+        <div className='rating-host-container'>
+          <div>{logements.rating}
+          </div>
+          <div className="user-container">
+            <div className="user-container_name">{logements.host.name}</div>
+            <img
+              src={logements.host.picture}
+              alt="User profil"
+              className="user-picture"
+            />
           </div>
         </div>
       </div>
