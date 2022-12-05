c4e30dff409fb6300432b45ee556ad3f50bad6a8
Kevin <kevin.le.milanais@hotmail.fr>
2022-12-05 17:39:05 +0100

    Modification Banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import banner from '../assets/banner.png'
 import '../styles/components/banner.scss'
 
 const Banner = () => {
   return (
     <div className="banner-container">
       <div className="banner-background">
+        <img className='banner-background_img' src={banner} alt="Bannière Accueil" />
         <h1 className="banner-background-title">
           Chez vous, Partout et ailleurs
         </h1>
